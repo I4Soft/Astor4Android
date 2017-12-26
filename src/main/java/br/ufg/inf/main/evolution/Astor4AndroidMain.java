932dd3f1c9891df2d9891ac760e151f6668dcad5
kayquesousa <kayque23@hotmail.com>
2017-12-26 18:02:36 -0200

    Fixed a problem with the task "saveDependencies"

--- src/main/java/br/ufg/inf/main/evolution/Astor4AndroidMain.java
+++ src/main/java/br/ufg/inf/main/evolution/Astor4AndroidMain.java
@@ -272,7 +272,7 @@ public class Astor4AndroidMain extends AstorMain {
 				ConfigurationProperties.getProperty("androidsdk") + "/extras/android/m2repository/"
 			  , ConfigurationProperties.getProperty("androidsdk") + "/extras/google/m2repository/" });
 
-		BufferedWriter out = new BufferedWriter(new FileWriter(location + "/build.gradle", true));
+		BufferedWriter out = new BufferedWriter(new FileWriter(location + "/app/build.gradle", true));
 
 		out.write("\n\nrepositories {");
 		for(String repository : m2repositories)
@@ -290,7 +290,7 @@ public class Astor4AndroidMain extends AstorMain {
 
    		CommandExecutorProcess.execute("./gradlew saveDependencies -no-daemon", location);
 
-   		extractAAR(location + "/localrepo");
+   		extractAAR(location + "/app/localrepo");
 	}
 
 
